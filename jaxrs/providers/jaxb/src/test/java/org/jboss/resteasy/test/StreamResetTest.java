ecb93f22f612f9c2ce8732c7d986e613309d377c
kanovotn <kanovotn@redhat.com>
2015-12-10 11:10:41 +0100

    New client tests

--- jaxrs/providers/jaxb/src/test/java/org/jboss/resteasy/test/StreamResetTest.java
+++ jaxrs/providers/jaxb/src/test/java/org/jboss/resteasy/test/StreamResetTest.java
@@ -9,6 +9,10 @@ import org.junit.Test;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -18,107 +22,129 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
  */
-public class StreamResetTest extends BaseResourceTest
-{
-   @Path("/test")
-   public static class SimpleResource
-   {
-      @GET
-      @Produces("application/xml")
-      public String get()
-      {
-         return "<person name=\"bill\"/>";
-      }
-   }
+public class StreamResetTest extends BaseResourceTest {
+    @Path("/test")
+    public static class SimpleResource {
+        @GET
+        @Produces("application/xml")
+        public String get() {
+            return "<person name=\"bill\"/>";
+        }
+    }
 
-   @XmlRootElement(name = "person")
-   @XmlAccessorType(XmlAccessType.PROPERTY)
-   public static class Person
-   {
-      private String name;
+    @XmlRootElement(name = "person")
+    @XmlAccessorType(XmlAccessType.PROPERTY)
+    public static class Person {
+        private String name;
 
-      @XmlAttribute
-      public String getName()
-      {
-         return name;
-      }
+        @XmlAttribute
+        public String getName() {
+            return name;
+        }
 
-      public void setName(String name)
-      {
-         this.name = name;
-      }
-   }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 
-   @XmlRootElement(name = "place")
-   @XmlAccessorType(XmlAccessType.PROPERTY)
-   public static class Place
-   {
-      private String name;
+    @XmlRootElement(name = "place")
+    @XmlAccessorType(XmlAccessType.PROPERTY)
+    public static class Place {
+        private String name;
 
-      @XmlAttribute
-      public String getName()
-      {
-         return name;
-      }
+        @XmlAttribute
+        public String getName() {
+            return name;
+        }
 
-      public void setName(String name)
-      {
-         this.name = name;
-      }
-   }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 
-   @Before
-   public void setUp() throws Exception
-   {
-      addPerRequestResource(SimpleResource.class);
-   }
+    @Before
+    public void setUp() throws Exception {
+        addPerRequestResource(SimpleResource.class);
+    }
 
-   @Test
-   public void test456() throws Exception
-   {
-      ClientRequest request = new ClientRequest(TestPortProvider.generateURL("/test"));
-      ClientResponse<Place> response = request.get(Place.class);
-      boolean exceptionThrown = false;
-      try
-      {
-         Place place = response.getEntity();
+    @Test
+    public void test456() throws Exception {
+        ClientRequest request = new ClientRequest(TestPortProvider.generateURL("/test"));
+        ClientResponse<Place> response = request.get(Place.class);
+        boolean exceptionThrown = false;
+        try {
+            Place place = response.getEntity();
 
-      }
-      catch (Exception e)
-      {
-         exceptionThrown = true;
-      }
-      Assert.assertTrue(exceptionThrown);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        Assert.assertTrue(exceptionThrown);
 
-      response.resetStream();
+        response.resetStream();
 
-      Person person = response.getEntity(Person.class);
-      Assert.assertNotNull(person);
-      Assert.assertEquals("bill", person.getName());
-   }
+        Person person = response.getEntity(Person.class);
+        Assert.assertNotNull(person);
+        Assert.assertEquals("bill", person.getName());
+    }
 
-   @Test
-   public void test456WithoutResetStream() throws Exception
-   {
-      ClientRequest request = new ClientRequest(TestPortProvider.generateURL("/test"));
-      ClientResponse<Place> response = request.get(Place.class);
-      boolean exceptionThrown = false;
-      try
-      {
-         Place place = response.getEntity();
+    @Test
+    public void test456WithoutResetStream() throws Exception {
+        ClientRequest request = new ClientRequest(TestPortProvider.generateURL("/test"));
+        ClientResponse<Place> response = request.get(Place.class);
+        boolean exceptionThrown = false;
+        try {
+            Place place = response.getEntity();
 
-      }
-      catch (Exception e)
-      {
-         exceptionThrown = true;
-      }
-      Assert.assertTrue(exceptionThrown);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        Assert.assertTrue(exceptionThrown);
 
-      //response.resetStream();
+        //response.resetStream();
+        Response response1;
 
-      Person person = response.getEntity(Person.class);
-      Assert.assertNotNull(person);
-      Assert.assertEquals("bill", person.getName());
-   }
+        Person person = response.getEntity(Person.class);
+        Assert.assertNotNull(person);
+        Assert.assertEquals("bill", person.getName());
+    }
+
+    @Test
+    public void test456NewClient() throws Exception {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(TestPortProvider.generateURL("/test"));
+        Response response = target.request().get();
+        boolean exceptionThrown = false;
+        try {
+            Place place = response.readEntity(Place.class);
+
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        Assert.assertTrue(exceptionThrown);
+
+        Person person = response.readEntity(Person.class);
+        Assert.assertNotNull(person);
+        Assert.assertEquals("bill", person.getName());
+    }
+
+    @Test
+    public void test456NewClientWithBuffer() throws Exception {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(TestPortProvider.generateURL("/test"));
+        Response response = target.request().get();
+        boolean exceptionThrown = false;
+        response.bufferEntity();
+        try {
+            Place place = response.readEntity(Place.class);
+
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        Assert.assertTrue(exceptionThrown);
+
+        Person person = response.readEntity(Person.class);
+        Assert.assertNotNull(person);
+        Assert.assertEquals("bill", person.getName());
+    }
 
 }
