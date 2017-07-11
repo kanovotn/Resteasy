219fcf49be0a94652ca0dbdbaf0d8628adec39be
kanovotn <kanovotn@redhat.com>
2017-07-11 11:04:28 +0200

    [RESTEASY-1591][RESTEASY-1685] Enhance ClientExceptionsTest

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/exception/resource/ClientExceptionsIOExceptionRequestFilter.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/exception/resource/ClientExceptionsIOExceptionRequestFilter.java
@@ -1,14 +1,13 @@
-package org.jboss.resteasy.test.client.resource;
+package org.jboss.resteasy.test.client.exception.resource;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
 import java.io.IOException;
 import java.lang.Exception;
 
-public class ClientExceptionsCustomClientRequestFilter implements ClientRequestFilter {
+public class ClientExceptionsIOExceptionRequestFilter implements ClientRequestFilter {
     @Override
     public void filter(ClientRequestContext requestContext) throws IOException {
-        //throw new IOException("client io");
-            throw new RuntimeException("client io");
+        throw new IOException("client io");
     }
 }
